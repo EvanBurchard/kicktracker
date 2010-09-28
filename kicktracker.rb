50faffb8532e6648bff0564a46b6e29b569e62a4
evanburchard <evan.burchard@gmail.com>
2010-09-27 20:47:10 -0400

    put everything under 'projects' dir.  added a couple of bits of info to the hash

--- kicktracker.rb
+++ kicktracker.rb
@@ -4,10 +4,12 @@ require 'rubygems'
 require 'mechanize'
 require 'pp'
 
+username = "eerichmond"
+project = "lets-sail-around-the-world-the-first-ever-communi"
 # URL to your Kickstarter project
-url = "http://www.kickstarter.com/projects/571943958/tempt1-and-eyewriter-art-by-eyes"
-
-filename = File.dirname(__FILE__)+'/results.yml'
+url = "http://www.kickstarter.com/projects/#{username}/#{project}?ref=search"
+directory = File.dirname(__FILE__)+"/projects/#{username}/#{project}/"
+filename = directory + "results-#{DateTime.now.to_s}.yml"
 storage = ( File.exists?(filename) ? YAML.load(File.open(filename, 'r')) : [] )
 
 agent = Mechanize.new
@@ -18,12 +20,21 @@ money = (page/'#moneyraised')
 counts = (money/'h5').map do |stat|
   stat.content.split("\n").select {|n| !n.nil? && !n.empty? }.compact
 end
+goal_date = (page/'#banner').map do |words|
+  words.content.select {|n| !n.nil? && !n.empty? }.compact
+end
+goal = counts[1][1].match(/of (.*) goal/)[1].gsub('$','').gsub(',','').to_i
+raised = counts[1][0].gsub(',','').gsub('$','').to_i
+
 
 totals = {
+  :goal_date => goal_date[0].to_s.strip,
   :time => DateTime.now.to_s,
   :backers => counts[0][0].to_i, 
-  :raised => counts[1][0].gsub(',','').gsub('$','').to_i, 
-  :goal => counts[1][1].match(/of (.*) goal/)[1].gsub('$','').gsub(',','').to_i,
+  :raised => raised, 
+  :goal => goal,
+  :cash_over_goal => -(goal - raised),
+  :cash_under_goal => goal - raised,
   :days_left => counts[2][0].to_i
 }
 
@@ -31,6 +42,7 @@ puts url
 pp totals
 storage << totals
 
+FileUtils.mkdir_p directory
 File.open(filename, 'w+') {|f| f.write(storage.to_yaml) }
 exit 0
 
